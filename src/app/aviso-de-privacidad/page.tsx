4115762132d41762a3669a944dd02b416ce0fe92
Perezcortes <perezcortes05prz@gmail.com>
2025-10-28 15:24:11 -0600

    Actualizacion en aviso de privacidad

--- src/app/aviso-de-privacidad/page.tsx
+++ src/app/aviso-de-privacidad/page.tsx
@@ -154,8 +154,17 @@ export default function PrivacyPolicy() {
                     <li>En nuestra página web.</li>
                   </ul>
 
-                  <p>Si usted considera que han sido vulnerados sus derechos respecto de la protección de datos personales, tiene el derecho de acudir a la autoridad correspondiente para defender su ejercicio. La autoridad es el Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI), su sitio web es: www.ifai.mx.</p>
-
+                  <p>
+                    Si usted considera que han sido vulnerados sus derechos respecto de la protección de datos personales, tiene el derecho de acudir a la autoridad correspondiente para defender su ejercicio. La autoridad es la Secretaría de Anticorrupción y Buen Gobierno, su sitio web es: {' '}
+                    <a
+                      href="https://www.gob.mx/buengobierno"
+                      className="text-blue-600 underline hover:text-[#bea151] transition-colors duration-200"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      https://www.gob.mx/buengobierno
+                    </a>
+                  </p>
                   <h3 className="dorado mt-4">Limitación de Uso y Divulgación de Información</h3>
                   <p>En POLIZA DE RENTAS, nos comprometemos a proteger su privacidad. El uso de la información que nos proporciona se limita estrictamente a la finalidad para la cual fue recabada.</p>
 
